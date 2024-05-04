79efba66d3f041a7d49a267ee88240baf0db78e5
David Crowley <53307335+physcrowley@users.noreply.github.com>
2024-05-04 18:20:27 +0000

    header

---
+++ modular.py
@@ -0,0 +1,16 @@
+"""
+Name: David Crowley
+Date: May the 4th be with you, 2024
+Description: Exemplar for the Functions practical
+
+This exemplar meets only basic requirements :
+- 3 implemented functions + main
+- no control structures or logical operators
+- no flowcharts for planned logic
+
+Scope: Only the name variable was declared global since it is
+referenced by intro, get_name, get_city and bye functions. All other
+variables are declared locally with information flowing as arguments
+or return values
+"""
+
